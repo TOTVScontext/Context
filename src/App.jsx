b3a07391a242b395c7fd1a8410a732d05525ed29
leeosilvp <leo965679@gmail.com>
2026-04-25 18:17:55 -0300

    feat: criando rota para /login e iniciando arquivo Login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Navigate to='/login' />} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/home' element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import '../css/login.css'
+
+const Login = () => {
+    return (
+        <main className="login-main">
+
+        </main>
+    )
+}
+
+export default Login
