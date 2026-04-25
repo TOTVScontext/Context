ce8ad3e22622cd5e8fe8a304dbb9e9816ae56a73
leeosilvp <leo965679@gmail.com>
2026-04-25 19:16:58 -0300

    feat: criando conteudo do lado esq. de Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,13 @@
 import '../css/login.css'
+import logoContextL from '../assets/svg/logo-context.svg'
 
 const Login = () => {
     return (
         <main className="login-main">
-
+            <section className='login-content-left'>
+                <img src={logoContextL} />
+                <p>Faça logon ou crie uma conta</p>
+            </section>
         </main>
     )
 }
