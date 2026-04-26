994ab549fd10982ad0307c36e800a538dcec57ad
leeosilvp <leo965679@gmail.com>
2026-04-26 14:28:31 -0300

    feat: criando Hook ConsoleBanner para exibir banner TOTVScontext

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import ChangeIcon from './hooks/ChangeIcon'
+import { ConsoleBanner } from "./hooks/ConsoleBanner"
 
 function App() {
-
+  
+  ConsoleBanner();
   ChangeIcon();
 
   return (
