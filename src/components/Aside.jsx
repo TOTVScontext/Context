909f4b19eafcd792c9cb2914e152c297a87dbba0
leeosilvp <leo965679@gmail.com>
2026-04-27 22:16:47 -0300

    feat: criando estrutura inicial de Aside

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,8 +1,30 @@
+import { LogOut } from "@geist-ui/icons"
+import { useUser } from "../hooks/useUser";
 
 const Aside = () => {
+
+    const { user } = useUser();
+
     return (
         <aside className="aside-main">
-
+            <section className="aside-nav">
+                <nav>
+                    <ul>
+                        <Link to=''></Link>
+                        <Link to=''></Link>
+                        <Link to=''></Link>
+                        <Link to=''></Link>
+                        <Link to=''></Link>
+                        <Link to=''></Link>
+                    </ul>
+                </nav>
+                <Link to=''></Link>
+                <Link to=''></Link>
+            </section>
+            <section>
+                <button><LogOut size={17} /></button>
+                <Link><img src={user?.profile?.photo} /></Link>
+            </section>
         </aside>
     )
 }
