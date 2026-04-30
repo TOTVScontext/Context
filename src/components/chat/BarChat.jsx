fa49ddc8fc25d31a489b8edf180e94d3cb43768a
leeosilvp <leo965679@gmail.com>
2026-04-30 12:18:08 -0300

    feat: criando BarChat em Chat

--- src/components/chat/BarChat.jsx
+++ src/components/chat/BarChat.jsx
@@ -1,8 +1,23 @@
+import { Plus, Send } from "@geist-ui/icons"
 
 const BarChat = () => {
-    return (
-        <main>
 
+    const autoResize = (el) => {
+        if (!el) return
+        el.style.height = '0px'
+        el.style.height = el.scrollHeight + 'px'
+    }
+
+    return (
+        <main className="bar-chat-main">
+            <textarea placeholder="Como posso ajudar você hoje?" rows={1} onInput={(e) => autoResize(e.target)} />
+            <section className="bar-chat-btns">
+                <button><Plus size={20} /></button>
+                <div>
+                    <p>NVIDIA nemotron-3</p>
+                    <button><Send size={20} /></button>
+                </div>
+            </section>
         </main>
     )
 }
