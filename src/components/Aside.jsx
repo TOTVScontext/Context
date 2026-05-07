ee7662228758264cb88c7bba686d4546319f338a
leeosilvp <leo965679@gmail.com>
2026-05-06 23:58:50 -0300

    feat: criando função para diminuir largura do Aside

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -2,21 +2,32 @@ import { Link, NavLink, useNavigate } from 'react-router-dom'
 import { useUser } from '../hooks/useUser'
 import { logout } from '../services/auth'
 import logo from '../assets/svg/logo-context.svg'
-import { Aperture, Archive, BookOpen, Calendar, Code, Grid, Home, LogOut, Settings, TrendingUp } from "@geist-ui/icons"
-
+import { Aperture, Archive, BookOpen, Calendar, Grid, Home, LogOut, Settings, TrendingUp } from "@geist-ui/icons"
+import { useEffect, useState } from 'react'
 
 const Aside = () => {
+    const { user } = useUser()
+    const navigate = useNavigate()
 
-    const { user } = useUser();
+    const [collapsed, setCollapsed] = useState(() => {
+        return localStorage.getItem('aside_open') === 'false'
+    })
+
+    useEffect(() => {
+        const handleAsideToggle = () => {
+            setCollapsed(localStorage.getItem('aside_open') === 'false')
+        }
+
+        window.addEventListener('aside-toggle', handleAsideToggle)
+
+        return () => {
+            window.removeEventListener('aside-toggle', handleAsideToggle)
+        }
+    }, [])
 
     const FirstName = () => {
-        return user?.profile?.name
-            .trim()
-            .split(/\s+/)
-            .filter(Boolean)[0] || '';
+        return user?.profile?.name?.trim().split(/\s+/).filter(Boolean)[0] || ''
     }
-
-    const navigate = useNavigate()
 
     const handleLogout = async (e) => {
         e.preventDefault()
@@ -31,10 +42,11 @@ const Aside = () => {
     }
 
     return (
-        <aside className="aside-main">
+        <aside className={`aside-main ${collapsed ? 'collapsed' : ''}`}>
             <header className='aside-header'>
                 <img src={logo} alt="TOTVScontext" />
             </header>
+
             <section className='aside-nav'>
                 <div className='aside-nav-scrool'>
                     <nav>
@@ -46,11 +58,13 @@ const Aside = () => {
                             <NavLink to='/calendar'><Calendar size={15} />Agenda</NavLink>
                         </ul>
                     </nav>
+
                     <nav>
                         <ul>
                             <NavLink to='/chat'><Aperture size={15} />Context AI</NavLink>
                         </ul>
                     </nav>
+
                     <nav>
                         <ul>
                             <NavLink to='/integration'><Grid size={15} />Integrações</NavLink>
@@ -58,15 +72,20 @@ const Aside = () => {
                         </ul>
                     </nav>
                 </div>
+
                 <div className='aside-nav-profile'>
                     <Link to='/profile'>
-                        <img src={user?.profile?.photo} />
+                        <img src={user?.profile?.photo} alt={FirstName()} />
+
                         <div>
                             <h1>{FirstName()}</h1>
                             <h2>{user?.profile?.position}</h2>
                         </div>
                     </Link>
-                    <button onClick={handleLogout}><LogOut size={15} /></button>
+
+                    <button onClick={handleLogout}>
+                        <LogOut size={15} />
+                    </button>
                 </div>
             </section>
         </aside>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,43 @@
 import { Link } from 'react-router-dom'
-import { Bell, Code, HelpCircle, MessageCircle, Search, Sidebar, Terminal } from '@geist-ui/icons';
+import { Bell, Code, HelpCircle, MessageCircle, Search, Sidebar, Terminal } from '@geist-ui/icons'
 
 const Header = () => {
-    return (
-        <header className="header-main">
-            <section className='header-left'>
-                <button><Code size={14} /></button>
+  const handleToggleAside = () => {
+    const currentValue = localStorage.getItem('aside_open')
 
-                <article className='header-search'>
-                    <button><Search size={17} /></button>
-                    <input type="text" placeholder='Pesquisar (ctrl + p)' />
-                </article>
-            </section>
-            <section className='header-right'>
-                <Link to=''><HelpCircle size={15} />Ajuda</Link>
-                <Link className='active' to='https://github.com/TOTVScontext/Context-CLI/archive/refs/heads/main.zip'><Terminal size={15} />Context CLI</Link>
-            </section>
-        </header>
-    )
+    const isOpen = currentValue !== 'false'
+
+    localStorage.setItem('aside_open', (!isOpen).toString())
+
+    window.dispatchEvent(new Event('aside-toggle'))
+  }
+
+  return (
+    <header className="header-main">
+      <section className="header-left">
+        <button onClick={handleToggleAside}>
+          <Code size={14} />
+        </button>
+
+        <article className="header-search">
+          <button>
+            <Search size={17} />
+          </button>
+
+          <input type="text" placeholder="Pesquisar (ctrl + p)" />
+        </article>
+      </section>
+
+      <section className="header-right">
+        <Link to=''><HelpCircle size={15} />Ajuda</Link>
+
+        <Link className='active' to='https://github.com/TOTVScontext/Context-CLI/archive/refs/heads/main.zip'>
+          <Terminal size={15} />
+          Context CLI
+        </Link>
+      </section>
+    </header>
+  )
 }
 
 export default Header
