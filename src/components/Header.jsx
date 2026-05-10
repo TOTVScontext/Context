4a33e26aebcee4212994217ed52312a37e042caa
leeosilvp <leo965679@gmail.com>
2026-05-10 20:55:56 -0300

    fix: adicionando HTML de verificação do Google

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
             <section className="header-right">
                 <Link to=''><HelpCircle size={15} />Ajuda</Link>
 
-                <Link className='active'>
+                <Link to='' className='active'>
                     <Terminal size={15} />
                     Context CLI
                 </Link>
