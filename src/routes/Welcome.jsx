cbbe5c98774b570dacb6bafe7324f86b6a9aca2d
leeosilvp <leo965679@gmail.com>
2026-05-17 18:09:23 -0300

    feat: criando lado esq. da seção de apresentação de Welcome

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -1,6 +1,7 @@
 import '../css/welcome.css'
 import logo from '../assets/svg/logo-context.svg'
 import { Link } from 'react-router-dom'
+import { Chrome, Download } from '@geist-ui/icons'
 
 const Welcome = () => {
     return (
@@ -20,6 +21,21 @@ const Welcome = () => {
                     <Link to='/login?view=login' className='active'>Entrar</Link>
                 </div>
             </header>
+
+            <section className='welcome-content'>
+                <section className='welcome-presentation'>
+                    <article className='welcome-presentation-left'>
+                        <h1>Plataforma de inteligência conversacional para performance corporativa</h1>
+                        <p>O Context é uma plataforma de inteligência conversacional que transforma reuniões, chamadas e interações corporativas em métricas estruturadas, feedbacks acionáveis e inteligência estratégica de alta precisão.</p>
+                        <div>
+                            <Link className='active'><Chrome size={20} />Fazer LogIn</Link>
+                            <Link><Download size={20} />Baixar Context CLI</Link>
+                        </div>
+                    </article>
+                    <article className='welcome-presentation-right'>
+                    </article>
+                </section>
+            </section>
 
         </main>
     )
